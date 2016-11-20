e22fb177b961bbc758c37224c097f7ba47dd3826
JAMES-HP <u1257434@unimail.hud.ac.uk>
2016-11-20 22:58:49 +0000

    Added basic mapping to allow quick access to the wordnet dictionary via a url. Using /wordnet/{word}/{type} passes information to the wordnet service and system outs the information for now.

--- src/main/java/com/jm/controller/HomeController.java
+++ src/main/java/com/jm/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.jm.service.WordNetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -21,9 +22,10 @@ public class HomeController {
         return "home";
     }
 
-    @RequestMapping(value = "/wordnet")
-    public String tryWordNet() {
-        wordNetService.go();
+    @RequestMapping(value = "/wordnet/{word}/{type}")
+    public String tryWordNet(@PathVariable("word") String word,
+                             @PathVariable("type") String type) {
+        wordNetService.go(type, word);
         return "redirect:/";
     }
 
--- src/main/java/com/jm/service/WordNetService.java
+++ src/main/java/com/jm/service/WordNetService.java
@@ -3,44 +3,84 @@ package com.jm.service;
 import net.didion.jwnl.*;
 import net.didion.jwnl.data.*;
 import net.didion.jwnl.dictionary.Dictionary;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 import java.io.InputStream;
 
 /**
- * com.jm.service.WordNetService Class will...
+ * com.jm.service.WordNetService Class will manage all service queries to JWNL.
+ *
  * Created by James Millner on 15/11/2016 at 15:26.
  */
 @Service
 public class WordNetService {
 
+    private Log logger = LogFactory.getLog(this.getClass());
 
-    public void go() {
+    private static POS noun = POS.NOUN;
+    private static POS verb = POS.VERB;
+    private static POS adjective = POS.ADJECTIVE;
+    private static POS adverb = POS.ADVERB;
+
+    public void go(String type, String word) {
         initWordNet();
-        Dictionary dictionary = Dictionary.getInstance();
+
+        POS pos;
+        if(type.equals("noun"))
+            pos = POS.NOUN;
+        else if(type.equals("verb"))
+            pos = POS.VERB;
+        else if(type.equals("adjective"))
+            pos = POS.ADJECTIVE;
+        else if(type.equals("adverb"))
+            pos = POS.ADVERB;
+        else
+            pos = null;
 
         try {
-            IndexWord word = dictionary.lookupIndexWord(POS.NOUN, "wing");
-            System.out.println("Senses of the word 'wing':");
-            Synset[] senses = word.getSenses();
-
-            for (int i=0; i<senses.length; i++) {
-                Synset sense = senses[i];
-                System.out.println((i+1) + ". " + sense.getGloss());
-                Pointer[] holo = sense.getPointers(PointerType.PART_HOLONYM);
-                for (int j=0; j<holo.length; j++) {
-                    Synset synset = (Synset) (holo[j].getTarget());
-                    Word synsetWord = synset.getWord(0);
-                    System.out.print("  -part-of-> " + synsetWord.getLemma());
-                    System.out.println(" = " + synset.getGloss());
-                }
-            }
-
-        } catch (Exception a){
-            a.printStackTrace();
+            Synset[] response = searchDictionary(pos, word);
+            getMeaning(response, pos);
+        } catch (Exception e){
+            logger.fatal(e);
         }
     }
 
-    public void initWordNet() {
+    /**
+     * Method to take a type of word, and a word to then search the JWNL dictionary.
+     *
+     * If it finds a match return the array of results.
+     * @param type
+     * @param stringWord
+     * @return
+     * @throws JWNLException
+     */
+    public Synset[] searchDictionary(POS type, String stringWord) throws JWNLException {
+        Dictionary dictionary = Dictionary.getInstance();
+
+        IndexWord word = dictionary.lookupIndexWord(type, stringWord);
+        logger.info("Senses of the word 'door':");
+        return word.getSenses();
+    }
+
+    /**
+     * Method to take an array of descriptions supplied with from searching the WordNet dictionary.
+     * @param wordMeanings
+     * @param type
+     */
+    public void getMeaning(Synset[] wordMeanings, POS type) {
+        for (Synset sense : wordMeanings)
+        {
+            System.out.println(type + ": " + sense.getGloss());
+        }
+    }
+
+    /**
+     * Method to initial JWNL from the static file_properties file.
+     *
+     * Points JWNL to the correct dictionary.
+     */
+    private void initWordNet() {
         try {
             InputStream in = getClass().getResourceAsStream("/static/file_properties.xml");
             JWNL.initialize(in);
